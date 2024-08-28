e928dab7ddc1d9d27aafe52858cd891039acff2e
logossoudarryl20 <logossoudarryl20@gmail.com>
2024-08-28 09:13:50 +0000

    design de la page

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,7 +1,8 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:myapp/componenets/messsage_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,6 +14,8 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   late final GenerativeModel _model;
   late final ChatSession _chatSession;
+  // final FocusNode _textFieldFocusNode = FocusNode();
+  // final TextEditingController _textController = TextEditingController();
 
   @override
   void initState() {
@@ -27,7 +30,77 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(),
+      appBar: AppBar(
+        title: const Text("Build with Gemini"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  final Content content = _chatSession.history.toList()[index];
+                  final text = content.parts
+                      .whereType<TextPart>()
+                      .map<String>((e) => e.text)
+                      .join('');
+
+                  return MesssageWidget(
+                    text: text,
+                    isFromUser: content.role == 'user',
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: 25,
+                horizontal: 15,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      autofocus: true,
+                      focusNode: _textFieldFocusNode,
+                      decoration: textFieldDecoration(),
+                      controller: _textController,
+                      onSubmitted: _sendChatMessage,
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
+
+  // InputDecoration textFieldDecoration() {
+  //   return InputDecoration(
+  //     contentPadding: const EdgeInsets.all(15),
+  //     hintText: 'Enter a prompt',
+  //     border: OutlineInputBorder(
+  //       borderRadius: const BorderRadius.all(
+  //         Radius.circular(14),
+  //       ),
+  //       borderSide: BorderSide(
+  //         color: Theme.of(context).colorScheme.secondary,
+  //       ),
+  //     ),
+  //     focusedBorder: OutlineInputBorder(
+  //       borderRadius: const BorderRadius.all(
+  //         Radius.circular(14),
+  //       ),
+  //       borderSide: BorderSide(
+  //         color: Theme.of(context).colorScheme.secondary,
+  //       ),
+  //     ),
+  //   );
+  // }
 }
